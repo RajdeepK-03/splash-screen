47830808cf145d5f959d1d0275e560896bce84d1
simranjit singh <simranjitsingh1998>
2019-11-27 18:00:18 -0500

    dffrg

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    int SplashTime =4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mIntent= new Intent(MainActivity.this,NewActivity.class);
+                startActivity(mIntent);
+                finish();
+            }
+        },4000);
+    }
+}
